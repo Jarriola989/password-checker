740a53d0b841b5b33f2826f73e1ca85ef6190762
janeth <jarri023@fiu.edu>
2020-05-17 23:49:13 -0400

    cors

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 import checkpass
 
 app = Flask(__name__)
+
+cors = CORS(app)
+
 
 @app.route("/check-pass", methods=['POST'])
 def process_pass():
